ad9128da934566bac71b1c3efa0467b434f85535
Tiago Cunha <1700597@sal.ipg.pt>
2024-07-20 16:39:23 +0100

    adding metrics

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import random
 
 from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
+import prometheus_client
 import uvicorn
 
 app = FastAPI()
@@ -11,6 +12,19 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+heads_count = prometheus_client.Counter(
+    "heads_count",
+    "Number of heads",
+)
+tails_count = prometheus_client.Counter(
+    "tails_count",
+    "Number of tails",
+)
+flip_count = prometheus_client.Counter(
+    "flip_count",
+    "Number of flip",
 )
 
 @app.get("/flip-coins")
@@ -28,9 +42,20 @@ async def flip_coins(times=None):
             heads += 1
     tails = times_as_int - heads
 
+    heads_count.inc(heads)
+    tails_count.inc(tails)
+    flip_count.inc(times_as_int)
+
     return{
         "heads": heads,
         "tails": tails,
     }
+
+@app.get('/metrics')
+def get_metrics():
+    return Response(
+        media_type="text/plain",
+        content=prometheus_client.generate_latest(),
+    )
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
